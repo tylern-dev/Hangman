9a237d85b7cd53029425be148556c4527343edf1
Tyler Negro <TylerNegro@Tylers-MBP.lan>
2017-06-25 10:16:22 -0600

    letter checker working

---
+++ scripts/script.js
@@ -0,0 +1,20 @@
+var rightGuess = [];
+var wrongGuess = [];
+var wrongGuessText = document.getElementById('guess');
+var word = ['t','e','r','m','i','a','t','o','r']
+
+document.onkeyup = function(event){
+	var keyPressed = event.key;
+	
+	
+		// if (keyPressed === i){
+		// 	rightGuess.push(i)
+		}
+	}
+
+}
+
+// console.log(rightGuess);
+
+for (var i=0; i<word.length; i++){
+		console.log(i);
