b829fb645462a2e823f524423ed9e95626a423ef
Tyler Negro <TylerNegro@Tylers-MBP.lan>
2017-07-01 06:50:14 -0600

    maded changes to file

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,86 @@
-var rightGuess = [];
-var wrongGuess = [];
-var wrongGuessText = document.getElementById('guess');
-var word = ['t','e','r','m','i','a','t','o','r']
+//TODO:
+//invalid key check
+//new game generator
+//
+
+
+var wordList = [
+  'terminator',
+  'batman',
+  'superman',
+  'wonderwoman',
+  'deadpool',
+  'wolverine']
+var currentWord = wordList[Math.floor(Math.random()*wordList.length)];
+var keyPressed; //current letter pressed
+var knownLetters = '';
+var wrongLetters = [];
+var wrongGuessText = document.getElementById('wrong');
+var rightGuessText = document.getElementById('right');
+var guessesRemainingText = document.getElementById('guesses-remaining');
+var winCount = 0;
+var loseCount = 0; 
+var guessesRemaining = 9;
+var lettersArray;
+
+
+  for(var i = 0; i<currentWord.length; i++){
+      knownLetters += '_';
+  }
+  
+  lettersArray = knownLetters.split('');
+  
+ 
+  //this function allows for the individual letters to stay displayed. without it being a function, previous correct letters would disapear
+  function letterArrayFunc(key){
+    if (currentWord.indexOf(key) !== -1) {// is correct letter indexOf
+      for (var i = 0; i<lettersArray.length; i++){
+        if(key == currentWord[i]){
+            lettersArray[i] = key; 
+            gameWonCheck(lettersArray,currentWord);
+        
+        }
+      }
+    } 
+    else {
+      // letter is not correct
+      wrongLetters.push(key);
+      guessesRemaining -= 1;
+      gameOver(guessesRemaining)
+    }
+  }
+
+  //checks the array against the string to see if they match. If so, win
+  function gameWonCheck(lArray, cWord){
+    lArray=lArray.join('')
+    if(lArray===cWord){
+      alert('winner')
+    }
+  }
+
+  //checks wrong letter and counts down guesses
+  function gameOver(guess){
+    var playAgain;
+    if (guess === 0){
+      playAgain = confirm('Game Over! Play again?')
+      
+      if (playAgain){
+        loseCount += 1;
+        
+      }
+    }
+  }
+
 
 document.onkeyup = function(event){
-	var keyPressed = event.key;
-	
-	
-		// if (keyPressed === i){
-		// 	rightGuess.push(i)
-		}
-	}
+      keyPressed = event.key;
+      
+      letterArrayFunc(keyPressed);
+      
+      //reset game option?
+      //new game button?
 
-}
-
-// console.log(rightGuess);
-
-for (var i=0; i<word.length; i++){
-		console.log(i);
+      rightGuessText.innerHTML=lettersArray.join(' ');
+      wrongGuessText.innerHTML = wrongLetters.join(' ');  
+      guessesRemainingText.innerHTML = guessesRemaining;
+  }
